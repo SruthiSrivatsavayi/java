414740779f5edd467d08f50cf289f96bea7c9d25
SruthiSrivatsavayi <153825022+SruthiSrivatsavayi@users.noreply.github.com>
2024-08-01 15:26:58 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,9 @@
+public class Division
+{
+	public static void main(String args[]) 
+	{
+		int n1= 2;
+		int n2 = 4;
+		System.out.println("The division of the numbers is:"+(n1/n2));
+	}
+}	
---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+public class Multiplication
+{
+	public static void main(String args[]) 
+	{
+		int n1= 135;
+		int n2 = 230;
+		System.out.println("The sum of the numbers is:"+(n1*n2));
+	}
+}	
